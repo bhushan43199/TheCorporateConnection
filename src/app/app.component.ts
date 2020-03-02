4c8a388da0ffe0a146aadafc4b4cec9926f024e4
bhushan43199 <montupatidar43@gmail.com>
2020-03-02 11:20:56 +0530

    admin issue solve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MyserviceService } from './myservice.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'corporateConnection';
+
+  ngOnInit() {
+  
+  }
+
 }
